2f03992ee97a48458ce9653f658f49ad8d12cef7
Cesar Vinicio Rodas Alvarado <cvrodas@Desarrollo.administracion.local>
2020-02-12 09:36:43 -0600

    HT3

---
+++ src/ht3/Main.java
@@ -0,0 +1,11 @@
+package ht3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final String dir = System.getProperty("user.dir") + "\\";
+        System.out.println("current dir = " + dir );
+	}
+
+}
